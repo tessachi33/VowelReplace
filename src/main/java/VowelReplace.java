adf6fa7246f1615b26dd1a2b6057f539b8856982
Tessa <Tessachi33@yahoo.com>
2015-08-14 14:51:05 -0700

    added the bare bones routing after starting over in case of previous errors

--- src/main/java/VowelReplace.java
+++ src/main/java/VowelReplace.java
@@ -5,3 +5,25 @@ import java.io.Console;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+public class VowelReplace {
+   public static void main(String[] args){
+     String layout = "templates/layout.vtl";
+
+     get("/", (request, response) -> {
+       Map<String, Object> model = new HashMap<String, Object>();
+       model.put("template", "templates/input.vtl");
+       return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+
+     get("/output", (request, response) -> {
+       Map<String, Object> model = new HashMap<String, Object>();
+       model.put("template", "templates/output.vtl");
+       return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
+   }
+
+    
+
+
+}
