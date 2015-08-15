daf08d4af0eb5f114eb36947d9025c2603f221c7
Tessa <Tessachi33@yahoo.com>
2015-08-14 20:54:44 -0700

    Made String[] and a foreach loop, still getting errors and have not tested yet

--- src/main/java/VowelReplace.java
+++ src/main/java/VowelReplace.java
@@ -19,11 +19,37 @@ public class VowelReplace {
      get("/output", (request, response) -> {
        Map<String, Object> model = new HashMap<String, Object>();
        model.put("template", "templates/output.vtl");
+
+
        return new ModelAndView(model, layout);
      }, new VelocityTemplateEngine());
    }
 
-    
+//grabbing the user's input String
+public static String input = (String, userInput);
 
+//making the input String's letters lowercase
+String input = input.toLowerCase();
 
+//making the String's letters into an array
+String [] inputArray = input.split("");
+
+//going into the letter array and
+for (String letter : inputArray) {
+
+  if (letter == "a"){
+    System.out.println("-");
+  } else if (letter == "e"){
+    System.out.println("-");
+  } else if (letter == "i"){
+    System.out.println("-");
+  } else if (letter == "o"){
+    System.out.println("-");
+  } else if (letter == "u"){
+    System.out.println("-");
+  } else{
+    System.out.println(letter);
+  }
+
+}
 }
